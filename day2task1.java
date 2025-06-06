393bbb2b5e481e1a0da1695c779e3dda442aa4d7
karthikamurugesh6 <163977258+karthikamurugesh6@users.noreply.github.com>
2025-06-06 10:46:38 +0530

    Create day2task1.java

---
+++ day2task1.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class demo {
+	public static void details(String name , String Job ,String Package) {
+		System.out.println("My name is "+name+".My job is "+Job+".My package is "+Package);
+	}
+	public static void main(String[] args) {
+	details("karthika","developer","5LPA");
+	details("monika","fullstack developer","5LPA");
+	
+}
+}

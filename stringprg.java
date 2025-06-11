198c307e50e8cacffbde46e0085eb2948a75de20
karthikamurugesh6 <163977258+karthikamurugesh6@users.noreply.github.com>
2025-06-11 09:23:46 +0530

    Create stringprg.java

---
+++ stringprg.java
@@ -0,0 +1,14 @@
+package program;
+
+public class demo1 {
+	
+	
+	public static void Details(String name,String job,String Package) {
+		System.out.println("My Name is "+name+".My job is "+job+".My package is "+Package);
+	}
+	public static void main(String[] args) {
+		Details("karthika","Devops engineer","4LPA");
+		Details("monika","full stack developer","4LPA");
+	}
+
+}
